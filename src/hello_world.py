c54693b81d8c80f1f07ad9ed0d6d97a4c8713e0a
Jackie Dallas <jackie.r.dallas@gmail.com>
2025-01-21 16:04:39 -0500

    working with printing to the console

---
+++ src/hello_world.py
@@ -0,0 +1,18 @@
+"""
+hello_world.py
+@author: Jackie Johnson-Dallas
+Created January 21, 2025
+This script will display hello world on the console.
+"""
+
+
+def hello_world(name='New Student'):
+    """
+    Prints Hello World with a default name
+    or with a name passed to the function.
+    """
+    print(f"{name.title()}, Hello World from Python!")
+
+
+hello_world("jackie")
+hello_world()
