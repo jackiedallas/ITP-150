32953ac93d9a50cc900b81b3610fb0b012db972b
Jackie Dallas <jackie.r.dallas@gmail.com>
2025-01-28 15:33:30 -0500

    seconds_converter.py

--- src/PL_2/seconds_converter.py
+++ src/PL_2/seconds_converter.py
@@ -1,3 +1,22 @@
 """
-
+seconds_converter.py
+@author: Jackie Johnson-Dallas
+This script will calculate hours, minutes, and seconds of a given number
+then display it to the user.
 """
+
+# Get number of seconds from user
+total_seconds = float(input("Enter a number of seconds: "))
+
+# Get the number of hours
+hours = total_seconds // 3600
+
+# Get the number of remaining minutes
+minutes = (total_seconds // 60) % 60
+
+# Get the number of remaining minutes
+seconds = total_seconds % 60
+
+print(f"Hours: {hours}")
+print(f"Minutes: {minutes}")
+print(f"Seconds: {seconds}")
