6da65796c8f019ea3a10b6f56529e44ecb81b3cd
Jackie Dallas <jackie.r.dallas@gmail.com>
2025-03-20 23:30:42 -0400

    converted tuple1 to a list

--- src/PL_5/tuples_lab.py
+++ src/PL_5/tuples_lab.py
@@ -24,3 +24,24 @@ print(item1, item2, item3, item4)
 print('\n')
 
 print('Task 4. Add a new element with a value of 9 to tuple1 and see that a new object ID is created.')
+tuple1 = tuple1 + (9,)
+print('Contents of tuple1', tuple1)
+print('Object ID for tuple1', id(tuple1))
+print('Type of tuple1', type(tuple1))
+print('tuple1 at index 4 -->', tuple1[4])
+
+print('\n')
+
+print('Task 5. Add new items at specific indexes to tuple1')
+tuple1 = tuple1[:3] + (15, 20, 25) + tuple1[:5]
+print('Contents of tuple1 after adding the 15, 20, 25, and first 5 elements to the end of tuple1\n', tuple1)
+print('Object ID for tuple1', id(tuple1))
+
+print('\n')
+
+print('Task 6. Convert tuple1 to list1')
+list1 = list(tuple1)
+print('Contents of list1', list1)
+print('Object ID of list1', id(list1))
+
+print('\n')
