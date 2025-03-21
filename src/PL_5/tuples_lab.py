8a925a701b5f8e6945675f53bda167f3025e30d1
Jackie Dallas <jackie.r.dallas@gmail.com>
2025-03-20 22:47:22 -0400

    created tuple1

---
+++ src/PL_5/tuples_lab.py
@@ -0,0 +1,8 @@
+"""
+tuples_lab.py
+@author: Jackie Johnson-Dallas
+"""
+
+print('Task 1. Create a tuple1 with different data types.')
+tuple1 = ('tuple', False, 2.4, 1)
+print('Contents of tuple1:', tuple1)
