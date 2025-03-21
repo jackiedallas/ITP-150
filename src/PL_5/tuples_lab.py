1ed7a0e0d298777b3af2107ae7a1242ad62d5763
Jackie Dallas <jackie.r.dallas@gmail.com>
2025-03-20 22:57:04 -0400

    added variables for tuple elements

--- src/PL_5/tuples_lab.py
+++ src/PL_5/tuples_lab.py
@@ -3,6 +3,24 @@ tuples_lab.py
 @author: Jackie Johnson-Dallas
 """
 
+print('\n')
+
 print('Task 1. Create a tuple1 with different data types.')
 tuple1 = ('tuple', False, 2.4, 1)
 print('Contents of tuple1:', tuple1)
+print('Object ID for tuple1', id(tuple1))
+
+print('\n')
+
+print('Task 2. Access tuple1 element by its index')
+print('tuple1 at index 0 is', tuple1[0])
+
+print('\n')
+
+print('Task 3. Unpack tuple1 into several different variables.')
+item1, item2, item3, item4 = tuple1
+print(item1, item2, item3, item4)
+
+print('\n')
+
+print('Task 4. Add a new element with a value of 9 to tuple1 and see that a new object ID is created.')
