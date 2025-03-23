2f01c8c21c0c7f8ef00d599682dd50bcda2d6bce
Jackie Dallas <jackie.r.dallas@gmail.com>
2025-03-23 11:32:49 -0400

    imported random function

---
+++ src/PL_7/functions_lab.py
@@ -0,0 +1,10 @@
+"""
+functions_lab.py
+@author: Jackie Johnson-Dallas
+"""
+
+import random
+
+print('\nTask 1. Import a function.')
+integer = random.randint(0, 100)
+print(f'A random integer between 1 and 100 is {integer}.')
