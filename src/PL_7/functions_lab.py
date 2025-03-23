a59be0495c3c082665a1c1e64531b71c7c7db158
Jackie Dallas <jackie.r.dallas@gmail.com>
2025-03-23 11:35:54 -0400

    added directions for utilizing help function

--- src/PL_7/functions_lab.py
+++ src/PL_7/functions_lab.py
@@ -8,3 +8,7 @@ import random
 print('\nTask 1. Import a function.')
 integer = random.randint(0, 100)
 print(f'A random integer between 1 and 100 is {integer}.')
+
+print('\nTask 2. See the help.')
+print("If you want to see a function's documentation, enter help followed by the function name.")
+# help(random) # comment this out after running it the first time
