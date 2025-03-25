44d8b6b982c2b55cbc7370a8938630f947588aea
Jackie Dallas <jackie.r.dallas@gmail.com>
2025-03-25 15:10:19 -0400

    added positional arguments

--- src/PL_7/functions_lab.py
+++ src/PL_7/functions_lab.py
@@ -37,3 +37,21 @@ print('Call 1:')
 water(1, 'Smart Water', 2.69)
 print('Call 2 throws an error because we passed only 2 arguments.')
 # water(1, 'Dasani') # comment out after the first run
+print('Call 3 does not throw an error, but it is logically incorrect.')
+water('Nestle Pure Life', 2.69, 1) # logic error qty of 1 instead of price
+
+print('\nTask 6. Keyword Arguments allow flexibility')
+def water_keywords(qty, h2o, price):
+    print(f'{qty} {h2o} cost ${price:.2f}')
+
+print('\nCall 1. You must supply params')
+# water_keywords() # comment out after first run
+print('\nCall 2. Keyword Arguments in the same order as the function def.')
+water_keywords(qty=2, h2o='Dasani', price=2.69)
+print('\nCall 3. Keyword arguments not in the order of the def.')
+water_keywords(h2o='deer park', price=2.69, qty=3)
+print('Takeawaay. Using keyword arguments allows us to pass in different order')
+print('\nCall 4. Can we leave out an argument using keyword arguments? NO')
+# water_keywords(h2o='kirkland', price=3.56) # comment out after running first time
+print('\nCall 5. try a mix of positional and keyword arguments')
+water_keywords(2, h2o='Acqupanna', price=5.00)
